eef6975300041ab09e23b336db65b1896486e6c2
afterrain21-02 <tsashka51@gmail.com>
2024-10-24 16:18:31 +0700

    radioGroup, function calclateAndNavigate

--- Lab5/app/src/main/java/com/example/lab5/MainActivity.kt
+++ Lab5/app/src/main/java/com/example/lab5/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.example.lab5
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,13 +19,20 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var seekBarSale: SeekBar
     private lateinit var textViewSeekBar: TextView
+    private lateinit var editTextPrice: EditText
+    private lateinit var radioGroup: RadioGroup
+    private lateinit var ButtonOK: Button
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         seekBarSale = findViewById(R.id.seekBarSale)
         textViewSeekBar = findViewById(R.id.textViewSeekBar)
+        editTextPrice = findViewById(R.id.editTextPrice)
+        radioGroup = findViewById(R.id.RadioGroup)
+        ButtonOK = findViewById(R.id.buttonResult)
 
         seekBarSale.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBarSale: SeekBar?, progress: Int, fromUser: Boolean) {
@@ -34,6 +48,50 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+        ButtonOK.setOnClickListener {
+            calculateAndNavigate()
+        }
+    }
 
+    private fun calculateAndNavigate() {
+        val PriceRub = editTextPrice.text.toString().toDoubleOrNull()
+
+        if (PriceRub == null || PriceRub <= 0) {
+            Toast.makeText(this, "Пожалуйста, введите корректную цену в рублях", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val sale = seekBarSale.progress
+
+        val selectedRadioButton = radioGroup.checkedRadioButtonId
+
+        if (selectedRadioButton == -1) {
+            Toast.makeText(this, "Пожалуйста, выберите валюту.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val selectedCurrency = when (selectedRadioButton) {
+            R.id.radioButtonDollar -> "$"
+            R.id.radioButtonEuro -> "€"
+            R.id.radioButtonPound -> "£"
+            else -> "₽"
+        }
+
+        val discountedPrice = ((PriceRub / 100.0) * (100.0 - sale))
+
+        val convertedPrice = when (selectedCurrency) {
+            "Доллары" -> discountedPrice * 0.013
+            "Евро" -> discountedPrice * 0.012
+            "Фунты" -> discountedPrice * 0.010
+            else -> discountedPrice
+        }
+
+        // Передаем данные во вторую активность
+        val next = Intent(this, MainActivity2::class.java).apply {
+            putExtra("converted_price", convertedPrice)
+            putExtra("currency", selectedCurrency)
+        }
+
+        startActivity(next)
     }
 }
