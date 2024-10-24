491e46557f10e4bc8c7133f34b9fcd1c3301b8ae
afterrain21-02 <tsashka51@gmail.com>
2024-10-24 15:06:37 +0700

    seekBar

--- Lab5/app/src/main/java/com/example/lab5/MainActivity.kt
+++ Lab5/app/src/main/java/com/example/lab5/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.lab5
 
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var seekBarSale: SeekBar
+    private lateinit var textViewSeekBar: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        seekBarSale = findViewById(R.id.seekBarSale)
+        textViewSeekBar = findViewById(R.id.textViewSeekBar)
+
+        seekBarSale.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBarSale: SeekBar?, progress: Int, fromUser: Boolean) {
+                textViewSeekBar.text = "$progress%"
+            }
+
+            override fun onStartTrackingTouch(seekBarSale: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBarSale: SeekBar?) {
+
+            }
+        })
+
+
     }
 }
