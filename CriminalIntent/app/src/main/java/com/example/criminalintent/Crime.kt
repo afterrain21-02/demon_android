88c7077de08d6cc50c5adeb1324af8f3af0ded66
afterrain21-02 <tsashka51@gmail.com>
2024-11-08 16:28:13 +0700

    design MA2

--- CriminalIntent/app/src/main/java/com/example/criminalintent/Crime.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/Crime.kt
@@ -7,5 +7,6 @@ data class Crime(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
     @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "date") val date: Long,
-    @ColumnInfo(name = "is_solved") val isSolved: Boolean
+    @ColumnInfo(name = "is_solved") val isSolved: Boolean,
+    @ColumnInfo(name = "photo_path") val photoPath: String?
 )
