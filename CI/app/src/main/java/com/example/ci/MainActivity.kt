1d23c2d28fa96552f003dffe7909efb1bee1e799
afterrain21-02 <tsashka51@gmail.com>
2024-11-15 14:01:36 +0700

    First Start with toolbar

--- CI/app/src/main/java/com/example/ci/MainActivity.kt
+++ CI/app/src/main/java/com/example/ci/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.ci
 
+import CrimeFragment
+import android.graphics.Insets.add
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -12,5 +15,19 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.title = "Criminalintent"
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (currentFragment == null) {
+            val fragment = CrimeFragment()
+            supportFragmentManager
+                .beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                .commit()
+        }
     }
 }
