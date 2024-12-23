895caca286620121b8bcdbd1b09afa97bbb22d94
afterrain21-02 <tsashka51@gmail.com>
2024-12-23 19:44:28 +0700

    Beta version

--- Programming language choice/app/build.gradle.kts
+++ Programming language choice/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.programminglanguagechoice"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.programminglanguagechoice"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
