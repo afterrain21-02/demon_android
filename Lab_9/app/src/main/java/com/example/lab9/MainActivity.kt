ada9197f5d5d3ee4b147891170c4abcd047317fe
afterrain21-02 <tsashka51@gmail.com>
2024-12-26 22:44:01 +0700

    Main Activity

--- Lab_9/app/src/main/java/com/example/lab9/MainActivity.kt
+++ Lab_9/app/src/main/java/com/example/lab9/MainActivity.kt
@@ -5,16 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.room.InvalidationTracker
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var movieAdapter: MovieAdapter
+    private lateinit var movieViewModel: MovieViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Инициализация RecyclerView и ViewModel
+        movieAdapter = MovieAdapter()
+        recyclerView.adapter = movieAdapter
+        movieViewModel = ViewModelProvider(this).get(MovieViewModel::class.java)
+
+        // Наблюдение за данными
+        movieViewModel.allMovies.observe(this, InvalidationTracker.Observer { movies ->
+            movies?.let { movieAdapter.submitList(it) }
+        })
+
+        fab.setOnClickListener {
+            startActivity(Intent(this, AddActivity::class.java))
         }
     }
 }
